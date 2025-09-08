3097f3e3359ffef6d0334e9f1b3abf5ea49b37e1
MoEhab74 <mohamedehabswe74@gmail.com>
2025-09-08 14:53:43 +0300

    Create the ShopPage Drawer

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_e_commerce/pages/home_page.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
@@ -7,6 +8,43 @@ class ShopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      drawer: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        child: Column(
+          children: [
+             DrawerHeader(
+              child: Icon(
+                Icons.shopping_bag,
+                size: 140,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            const SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                title: Text(
+                  'Home',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+                onTap: () {
+                  // First close the drawer
+                  Navigator.pop(context);
+                  // Then push the home page
+                  Navigator.pushNamed(context, HomePage.routeName);
+                }
+              ),
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: const Text('Shop'),
         centerTitle: true,
