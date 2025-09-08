fc9bc42f2263727a96b9127ff72e49151c69026d
MoEhab74 <mohamedehabswe74@gmail.com>
2025-09-08 23:49:41 +0300

    Display Products in the ShopPage

--- lib/components/shop_products_builder.dart
+++ lib/components/shop_products_builder.dart
@@ -5,31 +5,37 @@ import 'package:minimal_e_commerce/cubits/shop_cubit/shop_cubit.dart';
 import 'package:minimal_e_commerce/cubits/shop_cubit/shop_states.dart';
 import 'package:minimal_e_commerce/models/product_model.dart';
 
-class ShopProductsBuilder extends StatelessWidget {
+class ShopProductsBuilder extends StatefulWidget {
   const ShopProductsBuilder({super.key});
 
   @override
+  State<ShopProductsBuilder> createState() => _ShopProductsBuilderState();
+}
+
+class _ShopProductsBuilderState extends State<ShopProductsBuilder> {
+  List<ProductModel> products = [];
+  @override
+  void initState() {
+    super.initState();
+    products= context.read<ShopCubit>().shopProducts;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ShopCubit, ShopState>(
-      builder: (context, state) {
-        // Get the products list from the cubit first
-        List<ProductModel> products = context.read<ShopCubit>().fetchShopData();
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              mainAxisSpacing: 16,
-              crossAxisSpacing: 16,
-              childAspectRatio: 0.75,
-              crossAxisCount: 2,
-            ),
-            itemBuilder: (context, index) {
-              return ProductItem(product: products[index],);
-            },
-            itemCount: products.length,
-          ),
-        );
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisSpacing: 16,
+          crossAxisSpacing: 16,
+          childAspectRatio: 0.75,
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (context, index) {
+          return ProductItem(product: products[index]);
+        },
+        itemCount: products.length,
+      ),
     );
   }
 }
--- lib/cubits/shop_cubit/shop_cubit.dart
+++ lib/cubits/shop_cubit/shop_cubit.dart
@@ -10,14 +10,21 @@ class ShopCubit extends Cubit<ShopState> {
     ProductModel(title: 'Product 2', price: 20, description: 'Lorem ipsum'),
     ProductModel(title: 'Product 3', price: 30, description: 'Lorem ipsum'),
     ProductModel(title: 'Product 4', price: 40, description: 'Lorem ipsum'),
+    ProductModel(title: 'Product 5', price: 50, description: 'Lorem ipsum'),
+    ProductModel(title: 'Product 6', price: 60, description: 'Lorem ipsum'),
+    ProductModel(title: 'Product 7', price: 70, description: 'Lorem ipsum'),
+    ProductModel(title: 'Product 8', price: 80, description: 'Lorem ipsum'),
+    ProductModel(title: 'Product 9', price: 90, description: 'Lorem ipsum'),
+    ProductModel(title: 'Product 10', price: 100, description: 'Lorem ipsum'),
   ];
   // unmodifiable to make it read only and protect from outside changes like add or remove
   List<ProductModel> get shopProducts => List.unmodifiable(_shopProducts);
+  
 
-  List<ProductModel> fetchShopData() {
-    emit(ShopLoading());
-    // await Future.delayed(const Duration(seconds: 2));
-    emit(ShopSuccess(_shopProducts));
-    return _shopProducts;
-  }
+  // List<ProductModel> fetchShopData() {
+  //   emit(ShopLoading());
+  //   // await Future.delayed(const Duration(seconds: 2));
+  //   emit(ShopSuccess(_shopProducts));
+  //   return _shopProducts;
+  // }
 }
