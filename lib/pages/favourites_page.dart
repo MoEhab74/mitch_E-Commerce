fcfb14a68af42dbd2e36f61b1150aab492525f79
MoEhab74 <mohamedehabswe74@gmail.com>
2025-09-10 01:14:35 +0300

    Finish FavouritesPage and display the data

--- lib/pages/favourites_page.dart
+++ lib/pages/favourites_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:minimal_e_commerce/components/products_builder.dart';
+import 'package:minimal_e_commerce/cubits/cart_cubit/cart_cubit.dart';
+import 'package:minimal_e_commerce/cubits/cart_cubit/cart_state.dart';
+import 'package:minimal_e_commerce/models/product_model.dart';
 
 class FavouritesPage extends StatelessWidget {
   const FavouritesPage({super.key});
@@ -9,9 +14,38 @@ class FavouritesPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Favourites Page'),
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          foregroundColor: Theme.of(context).colorScheme.secondary,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.secondary,
+      ),
+      body: BlocSelector<CartCubit, CartCubitState, List<ProductModel>>(
+        selector: (state) {
+          // هنا هنسحب بس الفيفوريتس
+          if (state is FavoritesUpdatedSuccessfully) {
+            return state.products;
+          }
+          return const [];
+          // return context.read<CartCubit>().favorites;
+        },
+        builder: (context, favorites) {
+          if (favorites.isEmpty) {
+            return const Center(
+              child: Text(
+                "Favourites is empty",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+              ),
+            );
+          }
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ProductsBuilder(
+              products: favorites,
+              cubitFunction: context.read<CartCubit>().toggleFavorite,
+              icon: Icons.delete,
+              snackBarMessage: "removed from Favorites",
+            ),
+          );
+        },
       ),
     );
   }
