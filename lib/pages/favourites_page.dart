704d2979cea94df97b98a18165273296fbd24afe
MoEhab74 <mohamedehabswe74@gmail.com>
2025-09-11 17:12:14 +0300

    Remove the unwanted bloc selector and depend on the Extention

--- lib/pages/favourites_page.dart
+++ lib/pages/favourites_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:minimal_e_commerce/components/products_builder.dart';
 import 'package:minimal_e_commerce/cubits/cart_cubit/cart_cubit.dart';
 import 'package:minimal_e_commerce/cubits/cart_cubit/cart_state.dart';
-import 'package:minimal_e_commerce/models/product_model.dart';
 
 class FavouritesPage extends StatelessWidget {
   const FavouritesPage({super.key});
@@ -18,33 +17,32 @@ class FavouritesPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         foregroundColor: Theme.of(context).colorScheme.secondary,
       ),
-      body: BlocSelector<CartCubit, CartState, List<ProductModel>>(
-        selector: (state) {
-          // هنا هنسحب بس الفيفوريتس
+      body: BlocBuilder<CartCubit, CartState>(
+        builder: (context, state) {
           if (state.isCartOrFavoritesUpdatedSuccessfully) {
-            return context.read<CartCubit>().favorites;
-          }
-          return const [];
-          // return context.read<CartCubit>().favorites;
-        },
-        builder: (context, favorites) {
-          if (favorites.isEmpty) {
-            return const Center(
-              child: Text(
-                "Favourites is empty",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+            final favorites = context.read<CartCubit>().favorites;
+
+            if (favorites.isEmpty) {
+              return const Center(
+                child: Text(
+                  "Favourites is empty",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                ),
+              );
+            }
+
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ProductsBuilder(
+                products: favorites,
+                cubitFunction: context.read<CartCubit>().toggleFavorite,
+                icon: Icons.delete,
+                snackBarMessage: "removed from Favorites",
               ),
             );
           }
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ProductsBuilder(
-              products: favorites,
-              cubitFunction: context.read<CartCubit>().toggleFavorite,
-              icon: Icons.delete,
-              snackBarMessage: "removed from Favorites",
-            ),
-          );
+
+          return const Center(child: CircularProgressIndicator());
         },
       ),
     );
