5807ac3fbf55477c19a5fadc8e4242f076e8582e
MoEhab74 <mohamedehabswe74@gmail.com>
2025-09-13 10:55:26 +0300

    Add timer to search text field

--- lib/components/search_text_field.dart
+++ lib/components/search_text_field.dart
@@ -1,31 +1,49 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:minimal_e_commerce/cubits/shop_cubit/shop_cubit.dart';
 
-class SearchTextField extends StatelessWidget {
-  const SearchTextField({
-    super.key,
-  });
+class SearchTextField extends StatefulWidget {
+  const SearchTextField({super.key});
+
+  @override
+  State<SearchTextField> createState() => _SearchTextFieldState();
+}
+
+class _SearchTextFieldState extends State<SearchTextField> {
+  Timer? timer;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       onChanged: (value) {
         // Trigger search method here
-        context.read<ShopCubit>().searchProduct(value);
+        if (timer != null) {
+          timer!.cancel();
+        }
+        timer = Timer(Duration(milliseconds: 500), () {
+          context.read<ShopCubit>().searchProduct(value);
+        });
       },
       decoration: InputDecoration(
         hintText: 'Search',
         hintStyle: TextStyle(color: Theme.of(context).colorScheme.secondary),
         enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary, width: 1.5),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.primary,
+            width: 1.5,
+          ),
           borderRadius: BorderRadius.circular(16),
         ),
         focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary, width: 1.5),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.primary,
+            width: 1.5,
+          ),
           borderRadius: BorderRadius.circular(16),
         ),
-      )
+      ),
     );
   }
 }
