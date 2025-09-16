cbfcf90405dc26e528f6004ef46cd0383cf59094
MoEhab74 <mohamedehabswe74@gmail.com>
2025-09-16 14:26:31 +0300

    Create login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+  static const routeName = 'LoginPage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        title: const Text('Login'),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.secondary,
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Theme.of(context).colorScheme.primary,
+              width: 1.5,
+            ),
+            borderRadius: BorderRadius.circular(20),
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // userName textFormField
+              TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Theme.of(context).colorScheme.surface,
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                  prefixIcon: Icon(Icons.person),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 1.5),
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 1.5),
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                ),
+                
+              ),
+
+              // Password textFormField
+
+              // Login button
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
