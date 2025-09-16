920864f6320805ffbbaf7cd7e512982eb15faaad
MoEhab74 <mohamedehabswe74@gmail.com>
2025-09-17 00:32:43 +0300

    Create ProfilePage

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:minimal_e_commerce/cubits/auth/auth_cubit.dart';
+import 'package:minimal_e_commerce/models/user_model.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+  static const String routeName = 'ProfilePage';
+
+  @override
+  Widget build(BuildContext context) {
+    UserModel currentUser = context.read<AuthCubit>().getCurrentUser()!;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.secondary,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout_outlined),
+            onPressed: () {
+              // Trigger logout method here
+            },
+          ),
+        ],
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 14),
+            ClipOval(
+              child: Image.network(
+                currentUser.image,
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 12),
+            Text(
+              '${currentUser.firstName} ${currentUser.lastName}',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              currentUser.email,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Theme.of(context).colorScheme.onSurfaceVariant,
+              ),
+            ),
+            const SizedBox(height: 14),
+            
+          ],
+          ),
+        ),
+      )
+    );
+  }
+}
