3ebe81ebe369f463819f57fd93aa511c515c3e9e
MoEhab74 <mohamedehabswe74@gmail.com>
2025-09-16 04:10:01 +0300

    Refactor the UserModel to match the response data

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,23 +1,37 @@
 class UserModel {
-  final String id;
-  final String accessToken;
-  final String refreshToken;
-  final String userName;
+  final int id;
+  final String username;
   final String email;
-  final String password;
   final String? firstName;
   final String? lastName;
   final String? gender;
+  final String image;
+  final String accessToken;
+  final String refreshToken;
 
   UserModel({
     required this.id,
-    required this.accessToken,
-    required this.refreshToken,
-    required this.userName,
+    required this.username,
     required this.email,
-    required this.password,
     this.firstName,
     this.lastName,
     this.gender,
+    required this.image,
+    required this.accessToken,
+    required this.refreshToken,
   });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      username: json['username'],
+      email: json['email'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      gender: json['gender'],
+      image: json['image'],
+      accessToken: json['accessToken'],
+      refreshToken: json['refreshToken'],
+    );
+  }
 }
