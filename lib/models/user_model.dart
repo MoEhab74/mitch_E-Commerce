585cd732c56730379c690645618002f9f4a585f7
MoEhab74 <mohamedehabswe74@gmail.com>
2025-09-16 16:13:32 +0300

    Generate the TypeAdapter for the UserModel

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,12 +1,26 @@
-class UserModel {
+import 'package:hive/hive.dart';
+
+part 'user_model.g.dart';
+
+@HiveType(typeId: 0)
+class UserModel extends HiveObject {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final String username;
+  @HiveField(2)
   final String email;
+  @HiveField(3)
   final String? firstName;
+  @HiveField(4)
   final String? lastName;
+  @HiveField(5)
   final String? gender;
+  @HiveField(6)
   final String image;
+  @HiveField(7)
   final String accessToken;
+  @HiveField(8)
   final String refreshToken;
 
   UserModel({
